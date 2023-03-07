4043962f12d96dfe6f57d4a11431e97680cc31cc
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-07 19:42:10 +0530

    Updating roboshop-jenkins-shared-library

---
+++ vars/ci.groovy
@@ -0,0 +1,36 @@
+def call() {
+
+    pipeline {
+
+        agent {
+            label 'workstation'
+        }
+        stages{
+
+            stage('compile/Build') {
+                steps {
+                    echo 'compile'
+                }
+            }
+
+            stage('Unit Tests') {
+                steps {
+                    echo 'Unit Tests'
+                }
+            }
+
+            stage('Quality Control') {
+                steps {
+                    echo 'Quality Control'
+                }
+            }
+            stage('Upload Code to Centralized Place') {
+                steps {
+                    echo 'Upload'
+                }
+            }
+        }
+
+    }
+
+}
