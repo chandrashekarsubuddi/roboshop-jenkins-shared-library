4b8ee1c04c3c408fe6c1d42d1cb0e3d5e9e9856a
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-10 18:39:38 +0530

    Updating roboshop-jenkins-shared-library

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,4 +3,11 @@ def new1() {
     def xyz = "hello"
     print "xyz = ${xyz}"
     print "abc = ${abc}"
+
+    if ( abc = "Some Data") {
+        print "YES"
+    }
+    else {
+        print "NO"
+    }
 }
