40fb8464a173f93596f59c4861f569a7de1af2ae
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-13 14:13:57 +0530

    Updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -11,6 +11,7 @@ def compile() {
 
 def unittests() {
     if (app_lang == "nodejs") {
+//        Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
 //        sh 'npm test'
         sh 'echo Test Cases'
     }
