64e465c85058c464458dd5cd39fd016999f850c5
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-14 00:10:35 +0530

    Updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -26,5 +26,6 @@ def unittests() {
 
 
 def email(email_note) {
-    mail bcc: '', body: 'TEST\nNEW1\nNEW2', cc: '', from: 'chandrashekar185@gmail.com', replyTo: 'chandrashekar185@gmail.com', subject: 'TEST FROM JENKINS', to: 'chandrashekar185@gmail.com'
+    sh 'env'
+    mail bcc: '', body: 'Job Failed : ', cc: '', from: 'chandrashekar185@gmail.com', replyTo: 'chandrashekar185@gmail.com', subject: 'TEST FROM JENKINS', to: 'chandrashekar185@gmail.com'
 }
