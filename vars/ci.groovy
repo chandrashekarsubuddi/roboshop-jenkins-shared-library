1b940c9f172128c35b7537a2ccca165e9a5b4309
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-21 18:45:29 +0530

    Updating roboshop-jenkins-shared-library

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -36,7 +36,7 @@ def call() {
                             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
                                 println "Password = ${SONAR_PASS}"
                                 sh "echo sh password = ${SONAR_PASS}"
-                                sh "sonar-scanner -Dsonar.host.url=http://172.31.4.40:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                                sh "sonar-scanner -Dsonar.host.url=http://172.31.6.201:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                             }
                         }
 
