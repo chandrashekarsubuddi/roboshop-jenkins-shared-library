d6f384da01929e6ac7f105c03d2877be0f4855af
cshekar2k10 <cshekar2k10@gmail.com>
2023-03-29 12:37:49 +0530

    Updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -34,8 +34,13 @@ def email(email_note) {
 
 def artifactPush() {
     sh "echo ${TAG_NAME} >VERSION"
+
     if (app_lang == "nodejs") {
         sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
+    }
+
+    if (app_lang == "nginx") {
+        sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
     }
 
 
@@ -46,6 +51,6 @@ def artifactPush() {
 
 
     }
-    
+
 }
 
