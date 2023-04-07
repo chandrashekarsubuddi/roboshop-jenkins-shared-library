dd561c2b5c53e694bcc3d32065a7d72476f6ddd5
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-07 18:04:47 +0530

    Updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -5,6 +5,16 @@ def call() {
                 label 'workstation'
             }
         }
-        
+
+        parameters {
+                string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+        }
+        stages {
+            stage('Terraform init') {
+                steps {
+                    sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+                }
+            }
+        }
     }
 }
